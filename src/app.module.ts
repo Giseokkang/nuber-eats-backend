a776722128232126fa21541830bd44935a3db30f
Giseokkang <river.key93@gmail.com>
2023-09-13 15:40:28 +0900

    #14.5 Task Scheduling is Awesome

--- src/app.module.ts
+++ src/app.module.ts
@@ -26,6 +26,7 @@ import { OrderItem } from './orders/entities/order-item';
 import { CommonModule } from './common/common.module';
 import { PaymentsModule } from './payments/payments.module';
 import { Payment } from './payments/entities/payment.entity';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
@@ -98,6 +99,7 @@ import { Payment } from './payments/entities/payment.entity';
     OrdersModule,
     CommonModule,
     PaymentsModule,
+    ScheduleModule.forRoot(),
   ],
   controllers: [],
 })
